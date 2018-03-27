ff2c69aeb2ac71185891d7a5c40be95ef20cdf90
BIT Student <classroom@bgit.rs>
2018-03-27 17:59:46 +0200

    + button

--- src/app/pages/feedPage/FeedPage.js
+++ src/app/pages/feedPage/FeedPage.js
@@ -9,16 +9,23 @@ class FeedPage extends Component {
     render() {
         return (
             <React.Fragment>
-            <Header/>   
-            <div className="container">
- 
-<FeedListImage/>
-<FeedListVideo/>
-<FeedListPost/>
+                <Header />
+                <div className="container">
+
+                    <FeedListImage />
+                    <FeedListVideo />
+                    <FeedListPost />
 
 
-            </div>
-            <Footer/>
+
+                </div>
+                <div className="fixed-action-btn direction-top">
+                    <a className="btn-floating btn-large red">
+                        <i className="material-icons">+</i>
+                    </a>
+
+                </div>
+                <Footer />
             </React.Fragment>
 
         );
