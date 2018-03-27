ded3be35b526341a41dcde501ab482ac60c9417b
BIT Student <classroom@bgit.rs>
2018-03-27 17:12:58 +0200

    feedPage

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FeedPage from './pages/feedPage/FeedPage';
 
-import { Switch, Route } from "react-router-dom";
-import WelcomePage from "";
+// import { Switch, Route } from "react-router-dom";
+// import WelcomePage from "";
 
 
 class App extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<Switch>
+
+				<FeedPage/>
+
+				{/* <Switch>
 					<Route exact path='/' component={WelcomePage} />
 					<Route exact path='/feeds' component={FeedPage} />
 					<Route exact path='/people' component={PeoplePage} />
 					<Route exact path='/profile' component={ProfilePage} />
 					<Route path='/feeds/:id' component={PostPage} />
-				</Switch>
+				</Switch> */}
 			</React.Fragment>
 		);
 	}
--- src/app/pages/feedPage/FeedPage.js
+++ src/app/pages/feedPage/FeedPage.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import FeedListImage from './FeedListItem/FeedListImage';
 import FeedListVideo from './FeedListItem/FeedListVideo';
 import FeedListPost from './FeedListItem/FeedListPost';
+import Header from '../../partials/Header';
+import Footer from '../../partials/Footer';
 
 class FeedPage extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
+<Header/>    
 <FeedListImage/>
 <FeedListVideo/>
 <FeedListPost/>
+<Footer/>
+
             </div>
         );
     }
