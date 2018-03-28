42346d1a6d13e41a6489256a741fd9b2b330973a
BIT Student <classroom@bgit.rs>
2018-03-28 10:18:02 +0200

    f1-feedPage finished

--- src/app/pages/feedPage/FeedPage.js
+++ src/app/pages/feedPage/FeedPage.js
@@ -2,20 +2,16 @@ import React, { Component } from 'react';
 import FeedListImage from './FeedListItem/FeedListImage';
 import FeedListVideo from './FeedListItem/FeedListVideo';
 import FeedListPost from './FeedListItem/FeedListPost';
-import Header from '../../partials/Header';
-import Footer from '../../partials/Footer';
 
 class FeedPage extends Component {
     render() {
         return (
             <React.Fragment>
-                <Header />
                 <div className="container">
 
                     <FeedListImage />
                     <FeedListVideo />
                     <FeedListPost />
-
 
 
                 </div>
@@ -25,7 +21,6 @@ class FeedPage extends Component {
                     </a>
 
                 </div>
-                <Footer />
             </React.Fragment>
 
         );
