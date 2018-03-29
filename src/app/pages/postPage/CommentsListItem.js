315a587b9d8514f5ba252a29e5bdf3f60509a3f2
BIT Student <classroom@bgit.rs>
2018-03-29 10:18:13 +0200

    getting comments

--- src/app/pages/postPage/CommentsListItem.js
+++ src/app/pages/postPage/CommentsListItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const CommentsListItem = () => {
+const CommentsListItem = (props) => {
     return (
 
         <div className="col s12 m8 offset-m2 l6 offset-l3">
@@ -11,11 +11,11 @@ const CommentsListItem = () => {
                     </div>
                     <div className="col s10">
                         <span className="black-text">
-                            This is a square image. Add the "circle" className to it to make it appear circular.
+                            {props.body}
               </span>
                     </div>
                 </div>
-                <p className="align-left">Jane</p>
+                <p className="align-left">{props.authorName}</p>
             </div>
         </div>
 
--- src/app/pages/postPage/TextPostPage.js
+++ src/app/pages/postPage/TextPostPage.js
@@ -7,48 +7,60 @@ class TextPostPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            postInfo: {}
+            postInfo: {},
+            commentInfo: [],
         }
 
     }
 
     componentDidMount() {
-        
+
         postsData.getTextPost(this.props.match.params.id)
-        .then(data => {
-            this.setState({
-                postInfo: data
+            .then(data => {
+                this.setState({
+                    postInfo: data
+                })
             })
-        })
-    } 
+        postsData.getCommentByPostId(this.props.match.params.id)
+            .then((response) => {
+                this.setState({
+                    commentInfo: response,
+                })
+            })
+    }
 
-    render() { 
+    render() {
+        console.log(this.state.commentInfo)
         return (
-        <div className="container">
-            <div className='col s12 m7'>
-            <p>{this.state.postInfo.text}</p>
-            </div>
-            <div class="row">
-                <form className="col s12">
-                    <div className="row align-center">
-                        <div className="input-field col s9">
-                            <input id="icon_prefix" type="text" className="validate" />
-                            <label htmlFor="icon_prefix">First Name</label>
+            <div className="container">
+                <div className='col s12 m7'>
+                    <p>{this.state.postInfo.text}</p>
+                </div>
+                <div class="row">
+                    <form className="col s12">
+                        <div className="row align-center">
+                            <div className="input-field col s9">
+                                <input id="icon_prefix" type="text" className="validate" />
+                                <label htmlFor="icon_prefix">First Name</label>
+                            </div>
+                            <div className="input-field col s3 ">
+                                <button className="btn waves-effect waves-light col s12 align-center" type="submit" name="action">
+                                    <i className="large material-icons">send</i>
+                                </button>
+                            </div>
                         </div>
-                        <div className="input-field col s3 ">
-                            <button className="btn waves-effect waves-light col s12 align-center" type="submit" name="action">
-                                <i className="large material-icons">send</i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
 
+                </div>
+                {
+                    this.state.commentInfo.map((el, i) => {
+                        return <CommentsListItem authorName={el.authorName} body={el.body} key={el.id} />
+                    })
+                }
             </div>
-            <CommentsListItem />
-            <CommentsListItem />
-        </div>
 
-    )}
+        )
+    }
 }
 
 export default TextPostPage;
