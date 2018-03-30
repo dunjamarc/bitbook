c304448e47c9fd21cd6c78fc939109b609bb4bf5
BIT Student <classroom@bgit.rs>
2018-03-30 13:07:05 +0200

    update profile not working

--- src/app/pages/profilePage/ModalUpdateProfile.js
+++ src/app/pages/profilePage/ModalUpdateProfile.js
@@ -1,8 +1,61 @@
 import React, { Component } from "react";
+import userService from '../../../services/userService.js';
 
 class ModalUpdateProfile extends Component {
 
-  
+    constructor(props) {
+        super(props);
+        this.state = {
+            nameInput: '',
+            descriptionInput: '',
+            avatarUrl: ''
+        };
+    }
+
+    // fetchData()
+
+    // componentDidMount() {
+
+    // }
+
+    handleImageChange = (event) => {
+        this.setState({
+            avatarUrl: event.target.value
+        })
+    }
+
+    handleNameChange = (event) => {
+        this.setState({
+            nameInput: event.target.value
+        })
+    }
+
+    handleDescriptionChange = (event) => {
+        this.setState({
+            descriptionInput: event.target.value
+        })
+    }
+
+    handleClick = (event) => {
+        const obj = {
+            name: this.state.nameInput,
+            about: this.state.descriptionInput,
+            avatarUrl: this.state.avatarUrl,
+            // email: 'nesto@gmail.com',
+            // aboutShort: 'about',
+            // userId: 391
+        };
+        userService.updateProfile(`http://bitbookapi.azurewebsites.net/api/Profiles`, obj)
+            .then((data) => {
+                this.props.value();
+                this.setState({
+                    nameInput: '',
+                    descriptionInput: '',
+                    avatarUrl: ''
+                })
+            })
+    }
+
 
     render() {
         return (
@@ -10,21 +63,25 @@ class ModalUpdateProfile extends Component {
                 <div className="modal-content">
                     <h4>Update profile</h4>
                     <img src="https://findd.com.my/include/img/user_dashboard/profile.png" alt="profile-image" />
+
                     <div className="file-field input-field">
                         <div className="btn">
                             <span>UPLOAD PHOTO</span>
-                            <input type="file" />
+                            <input type="file"/>
                         </div>
                         <div className="file-path-wrapper">
-                            <input className="file-path" type="text" />
+                            <input className="file-path" type="text" onChange={this.handleImageChange}/>
                         </div>
                     </div>
-                    <input type="text" placeholder="Full Name" />
-                    <textarea name="info" id="" cols="30" rows="10" placeholder="User description and all text that describes user"></textarea>
+
+                    <input type="text" placeholder="Full Name" onChange={this.handleNameChange} value={this.state.nameInput} />
+
+                    <textarea name="info" id="" cols="30" rows="10" placeholder="User description and all text that0 describes user" onChange={this.handleDescriptionChange} value={this.state.descriptionInput}></textarea>
                 </div>
+
                 <div className="modal-footer">
-                    <a href="#/profile" className="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
-                    <a href="#/profile" className="modal-action modal-close waves-effect waves-green btn-flat">Update</a>
+                    <a className="modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+                    <a onClick={this.handleClick} className="modal-action modal-close waves-effect waves-green btn-flat">Update</a>
                 </div>
             </form>
         )
